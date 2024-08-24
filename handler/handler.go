8b6fba4225a91036cee4f5117c56572cc3621a8d
mathewnicholls <mathewnicholls@yandex.com>
2024-08-24 19:20:58 +0100

    code review changes

--- handler/handler.go
+++ handler/handler.go
@@ -8,6 +8,7 @@ import (
 	"github.com/matnich89/trainstats-service-template/model"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 var upgrader = websocket.Upgrader{
@@ -32,7 +33,6 @@ func NewHandler(nrClient *client.NetworkRailClient) (*Handler, error) {
 }
 
 func (h *Handler) HandleNationalData(w http.ResponseWriter, r *http.Request) {
-	log.Println("yo")
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println("Error upgrading connection:", err)
@@ -49,11 +49,9 @@ func (h *Handler) HandleNationalData(w http.ResponseWriter, r *http.Request) {
 		select {
 		case data := <-h.realTimeDataChan:
 			log.Println("received data")
-			nrData := model.NationalData{
-				OnTime:              data.RTPPMDataMsgV1.RTPPMData.NationalPage.NationalPPM.OnTime,
-				CancelledOrVeryLate: data.RTPPMDataMsgV1.RTPPMData.NationalPage.NationalPPM.CancelVeryLate,
-				Late:                data.RTPPMDataMsgV1.RTPPMData.NationalPage.NationalPPM.Late,
-				Total:               data.RTPPMDataMsgV1.RTPPMData.NationalPage.NationalPPM.Total,
+			nrData, err := buildNationalRailData(data.RTPPMDataMsgV1.RTPPMData.NationalPage.NationalPPM)
+			if err != nil {
+				log.Println("Error building National Rail Data:", err)
 			}
 			b, err := json.Marshal(nrData)
 			if err != nil {
@@ -65,4 +63,34 @@ func (h *Handler) HandleNationalData(w http.ResponseWriter, r *http.Request) {
 			}
 		}
 	}
+}
+
+func buildNationalRailData(ppm realtime.NationalPPM) (model.NationalData, error) {
+	onTime, err := strconv.Atoi(ppm.OnTime)
+	if err != nil {
+		return model.NationalData{}, err
+	}
+
+	canclledOrVeryLate, err := strconv.Atoi(ppm.CancelVeryLate)
+	if err != nil {
+		return model.NationalData{}, err
+	}
+
+	late, err := strconv.Atoi(ppm.Late)
+	if err != nil {
+		return model.NationalData{}, err
+	}
+
+	total, err := strconv.Atoi(ppm.Total)
+	if err != nil {
+		return model.NationalData{}, err
+	}
+
+	return model.NationalData{
+		OnTime:              onTime,
+		CancelledOrVeryLate: canclledOrVeryLate,
+		Late:                late,
+		Total:               total,
+	}, nil
+
 }
--- model/model.go
+++ model/model.go
@@ -1,8 +1,8 @@
 package model
 
 type NationalData struct {
-	OnTime              string `json:"on_time"`
-	CancelledOrVeryLate string `json:"cancelled_or_very_late"`
-	Late                string `json:"late"`
-	Total               string `json:"total"`
+	OnTime              int `json:"on_time"`
+	CancelledOrVeryLate int `json:"cancelled_or_very_late"`
+	Late                int `json:"late"`
+	Total               int `json:"total"`
 }
